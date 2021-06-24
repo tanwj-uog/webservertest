547506735f42cc69402643d68e1391fe1add4cb0
Wei Jian <2508469t@student.gla.ac.uk>
2021-06-24 14:37:27 +0800

    Comment out stuff

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, jsonify, request
-import pyodbc
-import collections
-import json
+# import pyodbc
+# import collections
+# import json
 
 
-conn = pyodbc.connect('DRIVER={SQL Server};'
-                      'SERVER=tcp:csc3004.database.windows.net,1433;'
-                      'DATABASE=csc3004;UID=csc3004;PWD=Webcrawler3004;')
-cursor = conn.cursor()
+# conn = pyodbc.connect('DRIVER={SQL Server};'
+#                       'SERVER=tcp:csc3004.database.windows.net,1433;'
+#                       'DATABASE=csc3004;UID=csc3004;PWD=Webcrawler3004;')
+# cursor = conn.cursor()
 app = Flask(__name__)
 
 @app.route("/")
