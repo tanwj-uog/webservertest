53e3d1585eae549adefbcaf08e9b2ac2e5eb735e
Wei Jian <2508469t@student.gla.ac.uk>
2021-06-24 15:11:14 +0800

    indentation

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import json
 
 conn = pyodbc.connect('DRIVER={SQL Server};'
                       'SERVER=tcp:csc3004.database.windows.net,1433;'
-                     'DATABASE=csc3004;UID=csc3004;PWD=Webcrawler3004;')
+                      'DATABASE=csc3004;UID=csc3004;PWD=Webcrawler3004;')
 cursor = conn.cursor()
 app = Flask(__name__)
 
