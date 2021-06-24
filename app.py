bc4ef871f724a928c27dee6205ba66b041b6b3f9
Wei Jian <2508469t@student.gla.ac.uk>
2021-06-24 17:06:51 +0800

    Uncomment connection string db

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import collections
 import json
 
 
-# conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
-#                       'SERVER=tcp:csc3004.database.windows.net,1433;'
-#                       'DATABASE=csc3004;UID=csc3004;PWD=Webcrawler3004;')
-# cursor = conn.cursor()
+conn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};'
+                      'SERVER=tcp:csc3004.database.windows.net,1433;'
+                      'DATABASE=csc3004;UID=csc3004;PWD=Webcrawler3004;')
+cursor = conn.cursor()
 app = Flask(__name__)
 
 @app.route("/")
