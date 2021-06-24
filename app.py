5a079dfb5cf5e5decd88c86a99ce79304a482e32
Wei Jian <2508469t@student.gla.ac.uk>
2021-06-24 15:27:13 +0800

    Change driver string

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import collections
 import json
 
 
-conn = pyodbc.connect('DRIVER={SQL Server};'
+conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
                       'SERVER=tcp:csc3004.database.windows.net,1433;'
                       'DATABASE=csc3004;UID=csc3004;PWD=Webcrawler3004;')
 cursor = conn.cursor()
